7ecbb0b17bdf766a36d9d27b5a67f1ddd68e4ed7
shubham <5321007+shsethi@users.noreply.github.com>
2025-08-15 16:59:43 +0530

    local

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/kirti-gupta-folio-main/',
+  base: mode === 'production' ? '/kirti-gupta-folio-main/' : '/',
   server: {
     host: "::",
     port: 8080,
